9548c0be273466b94adf2285712aa735307df9fe
Jesse Pinho <jesse@jessepinho.com>
2016-12-13 01:18:07 +0100

    Customize the Touch Bar

--- macos/init.sh
+++ macos/init.sh
@@ -44,3 +44,7 @@ defaults write com.googlecode.iterm2 Hotkey -bool true
 defaults write com.googlecode.iterm2 HotkeyChar -int 126
 defaults write com.googlecode.iterm2 HotkeyCode -int 50
 defaults write com.googlecode.iterm2 HotkeyModifiers -int 1179914
+
+# Customize the icons in the Touch Bar
+defaults write com.apple.controlstrip MiniCustomized '( "com.apple.system.brightness", "com.apple.system.volume", "com.apple.system.mute", "com.apple.system.screen-lock" )'
+defaults write com.apple.controlstrip FullCustomized '( "com.apple.system.group.brightness", "com.apple.system.mission-control", "com.apple.system.group.keyboard-brightness", "com.apple.system.group.media", "com.apple.system.group.volume", "com.apple.system.screen-lock" )'
