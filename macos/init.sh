d51a982fb917ec7327a2c76364a878a242bcf8ad
Jesse Pinho <jesse@jessepinho.com>
2016-12-25 13:54:09 +0100

    Reorganize macos/init.sh

--- macos/init.sh
+++ macos/init.sh
@@ -2,58 +2,66 @@
 
 # Inspiration: https://mths.be/macos
 
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we’re about to change
-osascript -e 'tell application "System Preferences" to quit'
-
-# Use 24-hour time
-defaults write NSGlobalDomain AppleICUForce24HourTime -bool true
-
-# Format the menu bar clock: "Sun Jan 1 13:00"
-defaults write com.apple.menuextra.clock DateFormat "EEE MMM d H:mm"
-
-# Enable tap-to-click
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-
-# Enable three-finger drag
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
-
-# Use scroll gesture with the Ctrl (^) modifier key to zoom
-defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
-
-# When zoomed in, the screen image moves continuously with pointer
-defaults write ~/Library/Preferences/com.apple.universalaccess.plist closeViewPanningMode -int 0
-
-# Minimize windows using scale effect
-defaults write com.apple.dock mineffect scale
-
-# Minimize windows into application icon
-defaults write com.apple.dock minimize-to-application -bool true
-
-# Auto-hide the dock
-defaults write com.apple.dock autohide -bool true
-
-# Start SizeUp app at login
-defaults write com.irradiatedsoftware.SizeUp StartAtLogin -bool true
-
-# Make key repeat faster
-defaults write -g InitialKeyRepeat -int 15
-defaults write -g KeyRepeat -int 2
-
-# Require password immediately after sleep or screensaver begins
-defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-# Show the percentage in the battery menu item
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-
+# # iTerm2
 # Use Cmd + Shift + ~ keyboard shortcut for showing/hiding iTerm.
 defaults write com.googlecode.iterm2 Hotkey -bool true
 defaults write com.googlecode.iterm2 HotkeyChar -int 126
 defaults write com.googlecode.iterm2 HotkeyCode -int 50
 defaults write com.googlecode.iterm2 HotkeyModifiers -int 1179914
 
+# # Misc
+# Show the percentage in the battery menu item
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 # Customize the icons in the Touch Bar
 defaults write com.apple.controlstrip MiniCustomized '( "com.apple.system.screen-lock", "com.apple.system.volume", "com.apple.system.mute", "com.apple.system.dashboard" )'
 defaults write com.apple.controlstrip FullCustomized '( "com.apple.system.group.brightness", "com.apple.system.mission-control", "com.apple.system.dashboard", "com.apple.system.group.keyboard-brightness", "com.apple.system.group.media", "com.apple.system.group.volume", "com.apple.system.screen-lock" )'
+
+# # SizeUp
+# Start SizeUp at login
+defaults write com.irradiatedsoftware.SizeUp StartAtLogin -bool true
+
+# # System Preferences
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we're about to change
+osascript -e 'tell application "System Preferences" to quit'
+# ## Accessibility
+# ### Mouse & Trackpad
+# #### Trackpad Options...
+# Enable dragging (three finger drag)
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+# ### Zoom
+# Use scroll gesture with modifer keys to zoom: ^ Control
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+# #### Zoom style > Options...
+# When zoomed in, the screen image moves: Continuously with pointer
+defaults write ~/Library/Preferences/com.apple.universalaccess.plist closeViewPanningMode -int 0
+# ## Date & Time
+# ### Clock
+# Date options: Show the day of the week & Show date ("Sun Jan 1 13:00")
+defaults write com.apple.menuextra.clock DateFormat "EEE MMM d H:mm"
+# ## Dock
+# Minimize windows using: Scale effect
+defaults write com.apple.dock mineffect scale
+# Minimize windows into application icon
+defaults write com.apple.dock minimize-to-application -bool true
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+# ## Keyboard
+# ### Keyboard
+# Key Repeat
+defaults write -g KeyRepeat -int 2
+# Delay Until Repeat
+defaults write -g InitialKeyRepeat -int 15
+# ## Language & Region
+# Time format: 24-Hour Time
+defaults write NSGlobalDomain AppleICUForce24HourTime -bool true
+# ## Security & Privacy
+# ### General
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+# ## Trackpad
+# ### Point & Click
+# Tap to click
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
