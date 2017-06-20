1e7c1e537bffd902240d6a3ff08446c3e98414b5
Jesse Pinho <jesse@jessepinho.com>
2017-06-20 10:29:44 +0200

    Set the number of hours to show at a time in Calendar

--- macos/init.sh
+++ macos/init.sh
@@ -5,6 +5,12 @@
 # Keep-alive: update existing `sudo` time stamp until this script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# # Calendar
+# ## Preferences
+# ### General
+# Show: 24 hours at a time
+defaults write com.apple.iCal "number of hours displayed" 24
+
 # # Evernote
 # ## Preferences
 # ### General
